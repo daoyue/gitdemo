3b6fd0c6373ac993e51633125a181be6745c8be2
yanchen.yang@marketin.cn <yanchen.yang@marketin.cn>
2018-04-09 09:32:00 +0800

    #第四次测试，进行文件的新建操作

---
+++ src/main/java/com/example/gitdemo/micro/model/Demo2.java
@@ -0,0 +1,11 @@
+package com.example.gitdemo.micro.model;
+
+/**
+ * @author yanchen
+ * @data 2018-04-09
+ **/
+public class Demo2 {
+
+
+    private String name;
+}
---
+++ src/main/java/com/example/gitdemo/micro/model/UserInfo.java
@@ -0,0 +1,10 @@
+package com.example.gitdemo.micro.model;
+
+/**
+ * @author yanchen
+ * @data 2018-04-09
+ **/
+public class UserInfo {
+
+
+}
